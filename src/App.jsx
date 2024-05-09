010d97dd67cca7cb24862cb95090629ff229c523
mj <wmj0924@gmail.com>
2024-05-09 15:21:25 +0900

    객체만들기

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-function App() {
+import React from "react";
 
-  return (
-      <div>
+function App(props) {
+  // object : {}
+  //  {} 안에 property명 : property값 쌍들이 작성됨
+  let a = {};
+  let b = { name: "흥민" };
+  let c = { address: "신촌", city: "서울", country: "한국" };
 
-      </div>
-  );
+  console.log("a", a);
+  console.log("b", b);
+  console.log("c", c);
+
+  console.log("b의 name",b.name);
+  console.log("c의 address",c.address);
+
+  b.name = "강인";
+  console.log(b.name)
+
+  return <div></div>;
 }
 
 export default App;
