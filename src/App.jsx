6aecbe2342a5fdbb8ec68c2541bc90244e354456
mj <wmj0924@gmail.com>
2024-05-14 16:24:18 +0900

    children props

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
-import React, {useState} from 'react';
-
-function MyCounter() {
-    const [count, setCount] = useState(0);
-    return <div>
-        {count}
-        <button onClick={() => setCount(count + 1)}>증가</button>
-    </div>
-}
+import React from "react";
+import axios from "axios";
 
 function App(props) {
+    function handleClick1(){
+        axios.get("/path1");
+    }
+    function handleClick2(){
+        axios.get("/path2?city=서울&address=서울어딘가");
+    }
+    function handleClick3(){
+        axios.get("/path3?name=길동&age=26");
+    }
     return (
         <div>
-            <MyCounter/>
+            <div>
+                <button onClick={handleClick1}>/path1 get 요청</button>
+            </div>
+            <div>
+                <button onClick={handleClick2}>
+                    /path2 get 요청, city, address 요청 파라미터 첨부
+                </button>
+            </div>
+            <div>
+                <button onClick={handleClick3}>
+                    /path3 get 요청, name, age 요청 파라미터가 첨부
+                </button>
+            </div>
         </div>
     );
 }
